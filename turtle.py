61c838e86d43b0262d7263bef380fef1a5c2838a
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2022-02-01 18:49:30 +0700

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,21 @@
+#By:Rickthor7Ganz×SvgAraAra:v
+import turtle
+import colorsys
+
+t = turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+t.speed('0')
+n = 200
+h = 0 
+for i in range('360'):
+  c = colorsys.hsv_to_rgb(h, 1, 0.8)
+  h+= 1/n 
+  t.color(c)
+  t.left(2)
+  t.forward(i*2)
+  t.right(150)
+  for j in range (3)
+      t.rt(2)
+      t.forward(i*2)
+      t.lt(150)
